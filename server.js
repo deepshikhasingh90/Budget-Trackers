0a66f9cb4acc5d7f96a973447b98a88cb06e9c24
deep shikha <deepshikhasingh90@outlook.com>
2020-04-18 22:16:27 -0700

    adding mongo db Details

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetDb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
